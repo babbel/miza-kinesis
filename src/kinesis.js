c667eecdbc4f1e2d57580d71bd0fb0bf44fdc61a
Luca Bruzzone <spam@unsigned.it>
2023-05-31 10:44:11 +0200

    fix lint

--- src/kinesis.js
+++ src/kinesis.js
@@ -1,10 +1,19 @@
 const { KinesisClient } = require("@aws-sdk/client-kinesis");
 const { NodeHttpHandler } = require("@aws-sdk/node-http-handler");
 
-module.exports = ({ region, maxRetries, connectionTimeout, timeout, endpoint }) => {
+module.exports = ({
+  region,
+  maxRetries,
+  connectionTimeout,
+  timeout,
+  endpoint,
+}) => {
   const kinesisConfig = {
     region,
-    requestHandler: new NodeHttpHandler({ connectionTimeout, socketTimeout: timeout }),
+    requestHandler: new NodeHttpHandler({
+      connectionTimeout,
+      socketTimeout: timeout,
+    }),
     maxRetries,
   };
   if (endpoint) {
