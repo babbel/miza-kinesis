5ac3a6ba0c83952e61d31f720f2355152aa398cd
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2023-09-14 18:38:20 +0200

    run prettier

--- src/arnParser.js
+++ src/arnParser.js
@@ -1,4 +1,5 @@
-const ARN_VALIDATOR = /^arn:aws:kinesis:([a-z\-0-9]+):((http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(\d+)):stream\/(.+)$/;
+const ARN_VALIDATOR =
+  /^arn:aws:kinesis:([a-z\-0-9]+):((http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(\d+)):stream\/(.+)$/;
 
 const parse = (arn) => {
   const match = arn.match(ARN_VALIDATOR);
