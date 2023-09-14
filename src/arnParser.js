ed8e3007689121b561948c81ed76dfe3119ed97f
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2023-09-14 18:49:59 +0200

    add regex non-capturing to pass tests

--- src/arnParser.js
+++ src/arnParser.js
@@ -1,5 +1,5 @@
 const ARN_VALIDATOR =
-  /^arn:aws:kinesis:([a-z\-0-9]+):((http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(\d+)):stream\/(.+)$/;
+  /^arn:aws:kinesis:([a-z\-0-9]+):(?:(?:http(?:s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)|(?:\d+)):stream\/(.+)$/;
 
 const parse = (arn) => {
   const match = arn.match(ARN_VALIDATOR);
@@ -7,7 +7,7 @@ const parse = (arn) => {
 
   return {
     region: match[1],
-    resource: match[3],
+    resource: match[2],
   };
 };
 
