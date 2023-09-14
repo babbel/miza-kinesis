63226cec351cc339458086bd00f2876f51e95b01
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2023-09-14 18:34:37 +0200

    made URL validation regex pattern more robust 🍑 and re-introduced the existing numbers pattern

--- src/arnParser.js
+++ src/arnParser.js
@@ -1,4 +1,4 @@
-const ARN_VALIDATOR = /^arn:aws:kinesis:([a-z\-0-9]+):([a-zA-Z0-9:\/]+):stream\/(.+)$/;
+const ARN_VALIDATOR = /^arn:aws:kinesis:([a-z\-0-9]+):((http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(\d+)):stream\/(.+)$/;
 
 const parse = (arn) => {
   const match = arn.match(ARN_VALIDATOR);
